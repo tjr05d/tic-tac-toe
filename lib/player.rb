aa53a439d612c15338b75b1d80ecdc3a19723568
tjr05d <tjr05d@my.fsu.edu>
2017-05-04 10:46:58 -0400

    where we left off on 5.3

--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,26 @@
 module TicTacToe
   class Player
-  end 
+    attr_accessor :symbol, :name
+
+    def initialize(input)
+      raise ArgumentError.new("That's not a valid input") if input == {}
+      @name = input[:name]
+      @symbol = input[:symbol]
+    end
+
+    def cap_name
+      @name.capitalize
+    end
+  end
 end
+
+# attr_accessor :name, :symbol
+#
+# def initialize(name, symbol)
+#   @name = name
+#   @symbol = symbol
+# end
+tim = TicTacToe::Player.new({name: "Tim", symbol: "X"})
+p tim
+tim.symbol= "O"
+p tim
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,16 +4,26 @@ module TicTacToe
   describe Player do
 
     context "#initialize" do
-      it "raises an exception when initialized with {}" 
-      it "does not raise an error with a valid input hash"
+      it "raises an error when initialized with {}" do
+        expect{Player.new({})}.to raise_error
+      end
+
+      it "does not raise an error with a valid input hash" do
+        input = {name: "Tim", symbol: "X"}
+        expect{Player.new(input)}.to_not raise_error
+      end
     end
 
     context "#symbol" do
-      it "returns the players symbol"
+      it "returns the players symbol" do
+        input = {name: "Tim", symbol: "X"}
+        player = Player.new(input)
+        expect(player.symbol).to eq "X"
+      end
     end
 
     context "#name" do
-      it "returns the players names"
+      it "returns the players name"
     end
 
   end
